d1c4d4550c577268ccdb62c983b7d1c14cbf4055
Ruslan Negoda <103960102+RuslanNegoda1989Arjuna@users.noreply.github.com>
2023-01-15 16:34:52 +0200

    asdasds

--- src/components/TaskCounter/TaskCounter.jsx
+++ src/components/TaskCounter/TaskCounter.jsx
@@ -1,10 +1,22 @@
+import { useSelector } from "react-redux";
 import css from "./TaskCounter.module.css";
 
 export const TaskCounter = () => {
+  const tasks = useSelector(getTasks);
+  const count = tasks.reduce((acc, task) => {
+    if (task.completed) {
+      acc.completed += 1;
+    } else {
+      acc.active += 1;
+    }
+    console.log(acc);
+
+    return acc
+  }, {active: 0, copleted: 0})
   return (
     <div>
-      <p className={css.text}>Active: 0</p>
-      <p className={css.text}>Completed: 0</p>
+      <p className={css.text}>Active: {count.active}</p>
+      <p className={css.text}>Completed: {count.completed}</p>
     </div>
   );
 };
--- src/redux/selectors.jsx
+++ src/redux/selectors.jsx
@@ -1,5 +1,2 @@
-import { useSelector }  from "react-redux";
-
-const MyComponent = () => {
-    const value = useSelector(state => state.some.value)
-}
+export const getTasks = state => state.tasks;
+export const getStatusFilter = state => state.filters.status;
