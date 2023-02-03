af2e00ae69f00d32aeb6676db47e597a0a9187cb
Ruslan Negoda <103960102+RuslanNegoda1989Arjuna@users.noreply.github.com>
2023-02-03 09:36:01 +0200

    okokok

--- src/redux/reducer.jsx
+++ src/redux/reducer.jsx
@@ -1,62 +1,61 @@
+import { addTask, deleteTask, setStatusFilter, toggleCompleted } from "./actions";
 import { statusFilters } from "./constants";
+// Імпортуємо функцію композиції редюсерів
+// import { combineReducers } from "redux";
 
-const initialState = {
-  tasks: [
-    { id: 0, text: "Learn HTML and CSS", completed: true },
-    { id: 1, text: "Get good at JavaScript", completed: true },
-    { id: 2, text: "Master React", completed: false },
-    { id: 3, text: "Discover Redux", completed: false },
-    { id: 4, text: "Build amazing apps", completed: false },
-  ],
-  filters: {
-    status: statusFilters.all,
+
+const tasksInitialState = [
+  { id: 0, text: "Learn HTML and CSS", completed: true },
+  { id: 1, text: "Get good at JavaScript", completed: true },
+  { id: 2, text: "Master React", completed: false },
+  { id: 3, text: "Discover Redux", completed: false },
+  { id: 4, text: "Build amazing apps", completed: false },
+];
+
+// Відповідає лише за оновлення властивості tasks
+// Тепер значенням параметра state буде масив завдань
+export const tasksReducer = createReduser(tasksInitialState, {
+  [addTask]: (state, action) =>{
+    state.push(action.payload);
   },
+  [deleteTask]: (state, action) =>{
+    const index = state.findIndex(task => task.id === action.payload);
+    state.splice(index, 1);
+  },
+  [toggleCompleted]: (state, action) =>{
+    for (const task of state) {
+      if (task.id === action.payload) {
+        task.completed = !task.completed;
+      }
+    }
+  },
+});
+// export const tasksReducer = (state = tasksInitialState, action) => {
+//   switch (action.type) {
+//     case addTask.type:
+//       return [...state, action.payload];
+//     case deleteTask.type:
+//       return state.filter(task => task.id !== action.payload);
+//     case toggleCompleted.type:
+//       return state.map(task => {
+//         if (task.id !== action.payload) {
+//           return task;
+//         }
+//         return { ...task, completed: !task.completed };
+//       });
+//     default:
+//       return state;
+//   }
+// };
+
+const filtersInitialState = {
+  status: statusFilters.all,
 };
 
-// Використовуємо initialState як значення стану за умовчанням
-export const rootReducer = (state = initialState, action) => {
-  // Редюсер розрізняє екшени за значенням властивості type
-  switch (action.type) {
-    case 'tasks/addTask': {
-        return {
-            ...state,
-            tasks: [...state.tasks, action.payload],
-        }
-
-    }
-    case 'tasks/deleteTask': {
-        return {
-            ...state,
-            tasks: state.tasks.filter(task => task.id !== action.payload),
-        }
-
-    }
-    case "tasks/toggleCompleted":
-      return {
-        ...state,
-        tasks: state.tasks.map(task => {
-          if (task.id !== action.payload) {
-            return task;
-          }
-          return {
-            ...task,
-            completed: !task.completed,
-          };
-        }),
-      };
-      case "filters/setStatusFilter":
-      return {
-        ...state,
-        filters: {
-          ...state.filters,
-          status: action.payload,
-        },
-      };
-    // Залежно від типу екшену виконуватиметься різна логіка
-    default:
-      // Кожен редюсер отримує всі екшени, відправлені в стор.
-      // Якщо редюсер не повинен обробляти якийсь тип екшену,
-      // необхідно повернути наявний стан без змін.
-      return state;
+// Відповідає лише за оновлення властивості filters
+// Тепер значенням параметра state буде об'єкт фільтрів
+export const filtersReducer = createReduser(filtersInitialState, {
+  [setStatusFilter]: (state, action) => {
+    state.status = action.payload;
   }
-};
+});
--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,9 +1,20 @@
-import { createStore } from "redux";
-import { devToolsEnhancer } from "@redux-devtools/extension";
-import { rootReducer } from "./reducer";
+// import { createStore } from "redux";
+// import { devToolsEnhancer } from "@redux-devtools/extension";
+import {configureStore} from '@reduxjs/toolkit';
+import { filtersReducer, tasksReducer } from "./reducer";
+
+const store = configureStore({
+    reducer: {
+        task: tasksReducer,
+        filters: filtersReducer,
+    }
+})
+
+
+
 
 
 // Створюємо розширення стора, щоб додати інструменти розробника
-const enhancer = devToolsEnhancer();
+// const enhancer = devToolsEnhancer();
 
-export const store = createStore(rootReducer, enhancer);
+// export const store = createStore(rootReducer, enhancer);
